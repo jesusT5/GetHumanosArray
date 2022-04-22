8348f79addc804ae3f487330037c83739bc7c463
jtoledo5630@outlook.com <jtoledo5630@outlook.com>
2022-04-22 10:53:22 -0700

    Versión 1.0
    Subir todos los archivos por primera vez

---
+++ GetHumanosArray/Controllers/HumanoController.cs
@@ -0,0 +1,26 @@
+﻿using GetHumanoArray.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetHumanoArray.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HumanoController : ControllerBase
+    {
+        // GET: HumanoController
+        [HttpGet]
+        public Humano[] Index()
+        {
+            Humano[] humanos = {
+                new Humano(1, "Juan", "Masculino", 20, 1.60M, 70.60M) ,
+                new Humano(1, "Andres", "Masculino", 30, 1.70M, 80.5M) ,
+                new Humano(1, "Pedro", "Masculino", 25, 1.80M, 90) ,
+                new Humano(1, "Gustavo", "Masculino", 35, 1.60M, 85) ,
+                new Humano(1, "Pavel", "Masculino", 38, 1.65M, 63) ,
+                new Humano(1, "Carol", "Femenino", 25, 1.75M, 65) ,
+                new Humano(1, "Grecia", "Femenino", 26, 1.68M, 68) ,
+            };
+            return humanos;
+        }
+    }
+}
---
+++ GetHumanosArray/Model/Humano.cs
@@ -0,0 +1,23 @@
+﻿namespace GetHumanoArray.Model
+{
+    public class Humano
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Sexo { get; set; }
+        public int Edad { get; set; }
+        public decimal Altura { get; set; }
+        public decimal Peso { get; set; }
+
+        public Humano(int Id, string Nombre, string Sexo, int Edad, decimal Altura, decimal Peso)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.Sexo = Sexo;
+            this.Edad = Edad;
+            this.Altura = Altura;
+            this.Peso = Peso;
+
+        }
+    }
+}
